fdd8b87fd82596f0ffd137fca7cb790ad29d82e3
paturishhh <paige.eugenio@gmail.com>
2017-02-22 20:29:43 +0800

    Add files via upload
    flag implementation, circular queue nalang mismo kulang

--- serialQueue.ino
+++ serialQueue.ino
@@ -1,13 +1,20 @@
-void setup() {
-  Serial.begin(9600);
-//  printBuffer();
-}
 
-#define QUEUE_SIZE 0x05
-#define BUFFER_SIZE 30
+#define QUEUE_SIZE 0x08  // @ max 4
+#define BUFFER_SIZE 16
+
 boolean headerFound = false;
 static byte serialQueue[QUEUE_SIZE][BUFFER_SIZE];
 static byte serialBuffer[BUFFER_SIZE];
+//for queue
+byte head = 0x01; // 1 head = 0 AT ARRAY
+byte tail = 0x01;
+byte shiftCounter = 0x00;
+boolean isEmpty = true;
+
+void setup() {
+  Serial.begin(9600);
+  shiftCounter = 0x00;
+}
 
 void printBuffer(){
   for(byte y = 0x00; y < QUEUE_SIZE; y++){
@@ -16,8 +23,20 @@ void printBuffer(){
     }
     Serial.println();
   }
-  
 }
+
+void positionCounter(byte data){ // position in serial queue
+  Serial.print("D: ");
+  Serial.println(data,HEX);
+  while((data & 0x01) != 0x01){
+    shiftCounter = shiftCounter + 0x01;
+    data  = data >> 1;
+  }
+  Serial.print("S: ");
+  Serial.println(shiftCounter,HEX);
+}
+
+
 void loop() {
   // you can read one
   //trying serialQueue
@@ -36,20 +55,51 @@ void loop() {
       }
       if(serialData == 0xFE){
         serialBuffer[pos] = 0xFE; //adds footer
-        if(index != QUEUE_SIZE){ //if not max size
+        //check if header == tail and isempty // newly init
+        if(head == tail && isEmpty){
+          isEmpty = false;
+          positionCounter(tail);
           for(byte x = 0x00; x < pos; x++){
-            serialQueue[index][x] = serialBuffer[x]; //store data to perma queue
-//          Serial.println(serialQueue[index][x], HEX);
+            //store data to perma queue
+            serialQueue[shiftCounter][x] = serialBuffer[x]; 
           }
-          index++; 
-          pos = 0;
+          // shift tail
+          shiftCounter = 0x00;
+          tail = tail << 1;
           headerFound = false;
-          Serial.println(index); // patched index
-          printBuffer();
+        }
+        // header == tail !empty //full queue
+        else if(head == tail && isEmpty){
+          Serial.println("Full Queue");
         }
         else{
-          Serial.println("Max");
+          positionCounter(tail);
+          for(byte x = 0x00; x < pos; x++){
+            //store data to perma queue
+            serialQueue[shiftCounter][x] = serialBuffer[x]; 
+          }
+          shiftCounter = 0x00;
+          // shift tail
+          tail = tail << 1;
+          headerFound = false;
+          Serial.print("T: ");
+          Serial.println(tail,HEX);
         }
+//        printBuffer();
+        
+//        if(index != QUEUE_SIZE){ //if not max size
+//          for(byte x = 0x00; x < pos; x++){
+//            serialQueue[shiftCounter][x] = serialBuffer[x]; //store data to perma queue
+//          }
+//          index++; 
+//          pos = 0;
+//          headerFound = false;
+//          Serial.println(index); // patched index
+//          printBuffer();
+//        }
+//        else{
+//          Serial.println("Max");
+//        }
 //      }
 //      Serial.print(serialData,HEX);
     }
