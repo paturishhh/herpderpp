0db6c6fce08e5ae474c19667b55aa74126cf11fe
paturishhh <paige.eugenio@gmail.com>
2017-03-23 01:51:00 +0800

    Add files via upload
    pyserial and arduino test

---
+++ serialReader.py
@@ -0,0 +1,35 @@
+import serial, time
+arduino = serial.Serial('COM7', 9600, timeout=0.01)
+# bytes([0xFF,0x00,0x01,0x03, 0x00, 0xFE])
+# 'Magsend ka naaaa'.encode('utf-8')
+
+def convertStringToByteArray(packetCommand):
+	"Converts the string input into byte array read by node"
+	parsedCommand = packetCommand.split(" ")
+	convertedCommand = []
+
+	for x in parsedCommand:
+		# convertedCommand.append(bytes.fromhex(x))
+		byteVersion = bytes.fromhex(x);
+		convertedCommand.append(int.from_bytes(byteVersion, byteorder = 'little', signed = False))
+
+	print(convertedCommand)
+	# print(type(convertedCommand))
+	return convertedCommand
+
+packetCommand = input("Enter command: ")		
+	
+
+#convert input to bytes
+convertedCommand = convertStringToByteArray(packetCommand)
+
+arduino.write([convertedCommand[0]])
+# time.sleep(2)	
+arduino.write([convertedCommand[1]])
+# time.sleep(2)
+while True:
+	time.sleep(2)
+	# arduino.write(bytes([0xFF,0x00,0x01,0x03, 0x00, 0xFE])
+	data = arduino.readline()[:-2] #removes /r and /n
+	if data:
+		print (data)
---
+++ testing.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  Serial.println(Serial.available());
+  byte byteArray[0x01]; 
+  byte index = 0x00;
+  if(Serial.available() > 0){
+      byte val = Serial.read();
+//      Serial.print("Val");
+      Serial.println(val, HEX);
+      byteArray[index] = val;
+      index = index + 0x01;
+ 
+  }
+//  for(byte i = 0x00; i <= index ; i++){
+//    Serial.print(byteArray[i], HEX);
+//  }
+}
